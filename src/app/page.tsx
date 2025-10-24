6f639142319793c9c05081ad8e1ecd47008c7009
Anass <146187264+nasaou@users.noreply.github.com>
2025-10-24 10:52:08 +0100

    Add FeaturedProductsSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
+import FeaturedProductsSection from '@/components/FeaturedProductsSection';
 
 export default function HomePage() {
   return (
@@ -12,6 +13,7 @@ export default function HomePage() {
       
         <HeroSection />
         <AboutSection />
+        <FeaturedProductsSection />
       </main>
     </>
   );
